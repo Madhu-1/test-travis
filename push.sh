9a38186c303b1252a2d40e73f3f7cb6b13359019
Madhu  Rajanna <madhupr007@gmail.com>
2019-11-18 13:05:53 +0530

    enable DOCKER_CLI_EXPERIMENTAL
    Signed-off-by: Madhu  Rajanna <madhupr007@gmail.com>

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 export VERSION="latest"
 export DOCKER_REPO=madhupr001/multiarch
-
+export DOCKER_CLI_EXPERIMENTAL=enabled
 docker manifest inspect ${DOCKER_REPO}:${VERSION} >/dev/null && echo "Version ${VERSION} is already exists" && exit 0
 for ARCH_TYPE in amd64 arm64 arm; do
     if [ "$ARCH_TYPE" == "amd64" ]; then
